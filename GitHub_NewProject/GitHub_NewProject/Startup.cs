59c5404270c261a93d711555268c25c258ddc768
dhanatjega1306 <dhanatjega1306@hotmail.com>
2017-11-21 14:16:32 +0530

    hi
    hi

---
+++ GitHub_NewProject/GitHub_NewProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHub_NewProject.Startup))]
+namespace GitHub_NewProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
